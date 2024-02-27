2d982aaa6330e0ab5a904b0859bbc7b47e8d8529
Paulo Ricardo Sanches <368721@unoesc.lan>
2024-02-26 21:53:13 -0300

    Aula 26-02-24

--- Exercicios/240219_01/Program.cs
+++ Exercicios/240219_01/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// TRABALHANDO COM VARIAVEIS C#
+using System;
+// STRINGS
+// DECLARANDO VARIAVEL SEM INICIALIZACAO
+string message1 = null;
+// INICIALIZAR A VARIAVEL COM NULO
+string message2 = null;
+// INICIALIZAR STRING VAZIA
+string message3 = System.String.Empty;
+// DECLARAR UMA STRING COM VALOR IMPLICITO
+var message4 = "Uma mensagem aleatoria";
+
+Console.WriteLine( message4 );
+
+// CONCATENANDO STRINGS
+string concat = (message1 == null ? "" : message1) + " " + message2 + " " +
+                message3 + " " + message4;
+
+Console.WriteLine("\n" + concat );
+
+Console.WriteLine(
+     "A temperatura hoje {0:D} é {1}°C"
+    , DateTime.Now
+    , 23
+);
+
+string nome = string.Empty;
+Console.WriteLine("Qual é o seu nome? ");
+nome = Console.ReadLine();
+string resultado = $"Oi, {nome}! Pare de jaguarice!";
+Console.WriteLine(resultado);
---
+++ Exercicios/AtividadeAula2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+string nome = string.Empty;
+Console.WriteLine("Qual é o seu nome? ");
+nome = Console.ReadLine();
+
+string email = string.Empty;
+Console.WriteLine("Qual é o seu email? ");
+email = Console.ReadLine();
+
+string nascimento = string.Empty;
+Console.WriteLine("Qual a sua data de nascimento? ");
+nascimento = Console.ReadLine();
+
+string sexo = string.Empty;
+Console.WriteLine("Qual é o seu gênero? ");
+sexo = Console.ReadLine();
+
+string cep = string.Empty;
+Console.WriteLine("Qual é o cep da sua rua? ");
+cep = Console.ReadLine();
+
+string rua = string.Empty;
+Console.WriteLine("Qual é o nome da sua rua? ");
+rua = Console.ReadLine();
+
+string numCasa = string.Empty;
+Console.WriteLine("Qual é o número da sua casa? ");
+numCasa = Console.ReadLine();
+
+string bairro = string.Empty;
+Console.WriteLine("Qual é o seu bairro? ");
+bairro = Console.ReadLine();
+
+string cidade = string.Empty;
+Console.WriteLine("Qual é a sua cidade? ");
+cidade = Console.ReadLine();
+
+string uniFed = string.Empty;
+Console.WriteLine("Qual é a unidade federativa? ");
+uniFed = Console.ReadLine();
+
+string pais = string.Empty;
+Console.WriteLine("Qual é o seu país? ");
+pais = Console.ReadLine();
+
+string concat = nome + "\n" + email + "\n" + nascimento + "\n" +
+                sexo + "\n" + cep + "\n" + rua + "\n" + numCasa +
+                 "\n" + bairro + "\n"  + cidade + "\n" + uniFed +
+                  "\n"  + pais;
+Console.WriteLine("Os dados do usuario são os seguintes: \n");
+Console.WriteLine(concat);
+
